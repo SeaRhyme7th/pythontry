c2cd2bd316f6d143bc86c923b54f5a79d998cf8c
lee <airsunny@icloud.com>
2020-03-10 00:25:59 +0800

    @modify 做了点优化

--- parse-html.py
+++ parse-html.py
@@ -11,12 +11,12 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # print(r.status_code)
 # webHtml = r.text
 ## 先直接从本地获取
-file = open('./test.html', 'r');
-webHtml = file.read()
+# file = open('./test.html', 'r');
+# webHtml = file.read()
 # print(webHtml)
-
-
-# print(r.text)
+curlUrl = 'https://www.huya.com/688';
+allContent = requests.get(curlUrl);
+webHtml = allContent.text;
 
 parseHtmlSoup = BeautifulSoup(webHtml, 'lxml');
 # print(parseHtmlSoup.a)
@@ -25,32 +25,43 @@ scriptList = parseHtmlSoup.find_all('script')
 hrefList = [];
 for scriptTag in scriptList:
     temp = scriptTag.attrs
-    if 'href' in temp.keys():
-        hrefList.append(temp['href'])
-
-cssList = parseHtmlSoup.find_all('css')
+    if 'src' in temp.keys():
+        hrefList.append(temp['src'])
+cssList = parseHtmlSoup.find_all('link')
 for cssTag in cssList:
     temp = cssTag.attrs
-    if 'link' in temp.keys():
-        hrefList.append(temp['link']);
+    if 'href' in temp.keys():
+        hrefList.append(temp['href']);
 
+
+### 可以先将需要存储的js和css的资源存放到当前的目录中
+
+staticResourceFileName = "./tmp.txt";
+staticResourceFile = open(staticResourceFileName, "w");
 
 # start our request for these resource
 for urlList in hrefList:
+    staticResourceFile.write(urlList + "\n")
     # 先解析下应该保存的文件的名字
-    res = requests.get(url)
+    res = requests.get(urlList)
     if res.status_code == 200 :
         resourceTxt = res.text
-        if '?' in url :
+        if '?' in urlList :
             # 暂时不知道咋搞
             fileName = 'tmp';
         else :
-            splitUrl = url.split('/', -1);
-            fileName = splitUrl.pop
-        resourceFile = file.open(fileName, 'w');
-        print(resourceTxt, resourceFile);
-        resourceFile.close());
-
+            splitUrl = urlList.split('/', -1);
+            if len(splitUrl) > 0 :
+                data = requests.get(urlList)
+                fileName = splitUrl[-1]
+                resourceFile = open(fileName, 'w');
+                resourceFile.write(data.text)
+            # fileName = splitUrl.pop
+            # print(fileName)
+        # resourceFile = open(fileName, 'w');
+        # print(resourceTxt, resourceFile);
+        # resourceFile.close();
+staticResourceFile.close();
 
 # print(alist[0]);
 # print(alist[0].attrs);
